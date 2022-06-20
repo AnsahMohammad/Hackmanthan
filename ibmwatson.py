bf5b53193a5f0ec3ea0276e5fd7dc5c1f8ddceb4
gobbledy-gook <96362727+gobbledy-gook@users.noreply.github.com>
2022-06-21 01:43:25 +0530

    added keyword fn

--- ibmwatson.py
+++ ibmwatson.py
@@ -10,20 +10,52 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
     authenticator=authenticator
 )
 
-natural_language_understanding.set_service_url('{url')
+natural_language_understanding.set_service_url('{url}')
 
 
 def getEmotions(data):
     emotions = []
+    emotions_dict = {}
+    emotions_scores = []
     response_emo = natural_language_understanding.analyze(
-    text = data,
-    features = Features( emotion=EmotionOptions())
-    ).get_result() 
-
+                    text = data,
+                    features = Features( emotion=EmotionOptions())
+                    ).get_result() 
 
     for i in list(response_emo['emotion']['document']['emotion'].keys()):
         emotions.append([i, str(response_emo['emotion']['document']['emotion'][i])])
-    
-    return response_emo['emotion']['document']['emotion'], emotions    
+        emotions_scores.append(str(response_emo['emotion']['document']['emotion'][i]))
+
+    for i in list(response_emo['emotion']['document']['emotion'].keys()):
+        emotions_dict[i] = str(response_emo['emotion']['document']['emotion'][i])   
+
+    emotions_scores.sort()
+
+    for e in emotions:
+        print(e)
+        if emotions_scores[-1] == e[1]:
+            print('Check')
+            max_emo = e[0]    
+
+    return emotions, max_emo, emotions_dict    
+
+def getKeywords(data):
+    Keywords = []
+    Sentiments = []
+    score = 0
+    response_keyw = natural_language_understanding.analyze(
+                    text = data,
+                    features=Features(keywords=KeywordsOptions(sentiment=True, limit=10))).get_result()
+
+    i = 0
+    print('\n')
+    while i < len(response_keyw['keywords']):
+        Keywords.append(response_keyw['keywords'][i]['text'])
+        Sentiments.append(response_keyw['keywords'][i]['sentiment']['label'])
+        score += response_keyw['keywords'][i]['sentiment']['score']
+        i+=1
+        
+    return Keywords, Sentiments, score/(len(Keywords))
+    # return response_keyw['keywords']
 
 
