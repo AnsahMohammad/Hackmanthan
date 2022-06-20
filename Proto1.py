4405a51e1acefc2215603a7bdb32d2c72eb4cd2f
gobbledy-gook <96362727+gobbledy-gook@users.noreply.github.com>
2022-06-21 01:46:41 +0530

    Update Proto1.py
    more data will be shown in good format.

--- Proto1.py
+++ Proto1.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import ibmwatson as ibm
+import os
+import shutil
+from instaloader import Post
+import instaloader
 
 import json
 from ibm_watson import NaturalLanguageUnderstandingV1
@@ -7,7 +11,7 @@ from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 \
     import Features, CategoriesOptions, EmotionOptions, KeywordsOptions, EntitiesOptions
 
-authenticator = IAMAuthenticator('Uj0kpwPhaj99JT04MYpaAtOuOlLc4YoOMMNRu3hMS1Sc')
+authenticator = IAMAuthenticator('{api}')
 natural_language_understanding = NaturalLanguageUnderstandingV1(
     version='2022-04-07',
     authenticator=authenticator
@@ -15,17 +19,43 @@ natural_language_understanding = NaturalLanguageUnderstandingV1(
 
 st.header("S.W.A.G Tone Analyser")
 
-k = st.radio('Analyse Text from', ['Text', 'URL'])
-if k == 'Text':
-    data = st.text_input('Analyse Your Text')
+radioChoice = st.radio('Analyse Text from', ['Text', 'URL'])
+if radioChoice == 'Text':
+    data = st.text_area('Analyse Your Text')
 
     if st.button('Analyse'):
-        emotionsJson, emotionsList = ibm.getEmotions(data)
+        emotionsList, max_emotion, emo_dict = ibm.getEmotions(data)
         st.success('Your text is analysed!')
-    # st.json(emotionsJson)
-    # st.balloons()
         col1, col2, col3, col4, col5, col6 = st.columns(6)
+        
         for i in emotionsList:
             col1.write(i[0])
             col2.write(i[1])
         print(emotionsList)
+
+        keywords, sentiments, score = ibm.getKeywords(data)
+        for i in keywords:
+            col3.write(i)
+        for j in sentiments:    
+            col4.write(j)
+        col5.write('Sentiment Score')
+        col5.write(score)
+
+        if score < -0.7:
+            if max_emotion == 'joy' or emo_dict['joy'] > '0.55':
+                col6.write('Not HATEFUL')    
+            elif max_emotion == 'anger' and emo_dict['fear'] > '0.05' and emo_dict['disgust'] > '0.05':
+                col6.write('HATEFUL & Inflammatory')
+            elif max_emotion == 'sadness' and emo_dict['anger'] > '0.1':
+                col6.write('HATEFUL')
+            elif max_emotion == 'disgust':
+                if emo_dict['disgust'] > '0.2' and emo_dict['anger'] > '0.05':
+                    col6.write('HATEFUL')
+                elif emo_dict['anger'] > '0.1':
+                    col6.write('HATEFUL')  
+        elif emo_dict['disgust'] > '0.17' and emo_dict['anger'] > '0.05':
+            col6.write('HATEFUL')
+        elif max_emotion == 'sadness' and emo_dict['anger'] > '0.1':
+                col6.write('HATEFUL')    
+
+
